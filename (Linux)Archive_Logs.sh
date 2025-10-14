7a2810e894254e7d03df9d590034e3a330072bdc
Alex Liss <141281601+AliasUnknown710@users.noreply.github.com>
2025-10-14 16:12:09 -0400

    Update log directory paths in Archive_Logs.sh

--- (Linux)Archive_Logs.sh
+++ (Linux)Archive_Logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LOG_DIR="/home/alex/logs"
-ARCHIVE="/home/alex/logs/archive_$(date +%F).tar.gz"
+LOG_DIR="/home/username/logs"
+ARCHIVE="/home/username/logs/archive_$(date +%F).tar.gz"
 
 tar -czf "$ARCHIVE" "$LOG_DIR"/*.log
